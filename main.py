8d517cf965beebda86c74b9645957e4c1e4cb724
Jakub Januszewski <janir3g@gmail.com>
2022-05-03 13:29:45 +0200

    small visual change

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     graph = Graph(list_of_edges)
     functions = ['menu0.display()', 'menu1.display()', 'menu2.display()', 'menu3.display()', 'exit()']
     texts = ['Insert values', 'Get data from file', 'Create graphs / Operations on graphs', 'Create a random graph', 'Quit']
-    mainMenu = Menu(functions, texts, "Main menu", graph.edges)
+    mainMenu = Menu(functions, texts, "Main menu")
     mainMenu.display()
